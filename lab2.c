5abe75f1ab619b140eb743f4e99497a9ed9bf8c0
Atuchin01 <atuchin.s55@gmail.com>
2017-02-23 14:49:40 +0300

    Create input data

--- lab2.c
+++ lab2.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main (){
-	printf ("Hello world");
+	int s,t;
+	printf ("Vvedite summy vklada: "); 
+	scanf ("%i",&s);
+	printf ("Vvedite srok vklada: ");
+	scanf ( "%d",&t);
 	return 0;
 }
 
