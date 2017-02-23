e139c8ba9ba0cccd1bdea78e27d61aaab669b4e9
Atuchin01 <atuchin.s55@gmail.com>
2017-02-23 15:13:35 +0300

    Create fuction

--- lab2.c
+++ lab2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+int vklad(int a, int b);
 int main (){
 	int s,t;
 	printf ("Vvedite summy vklada: "); 
@@ -9,11 +9,34 @@ int main (){
 	if ((t <= 0) || (t > 365) || (s<10000))
 	printf ("VVedite coretnie dannie");
 	else {
-	printf (" Resultat vklada");
-		
-		
+	printf (" Resultat vklada: ");
+	printf ("%i", vklad(s,t));	
 	}
-	
 	return 0;
+}
+
+int vklad(int a, int b){
+int c=a;
+if (a<100000){
+if ((b > 0) && (b <= 30)) 
+        c*=0.9;
+        if ((b > 30) && (b <= 120)) 
+        c*=1.02;
+        if ((b > 120) && (b <= 240)) 
+        c*=1.06;
+         if ((b > 240) && (b <= 365)) 
+        c*=1.12;	
+}
+else {
+if ((b > 0) && (b <= 30)) 
+        c*=0.9;
+        if ((b > 30) && (b <= 120)) 
+        c*=1.03;
+        if ((b > 120) && (b <= 240)) 
+        c*=1.08;
+         if ((b > 240) && (b <= 365)) 
+        c*=1.15;	
+}
+return c;
 }
 
